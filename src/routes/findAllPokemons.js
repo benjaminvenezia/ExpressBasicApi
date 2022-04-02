f77de25cef55bab673a5a7b9271ea5c6fe6a1a97
benjamin <benjamin@pc-57.home>
2022-04-02 20:54:01 +0200

    add findPokemonByPk endpoint (primary key)

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,9 +1,9 @@
 const { Pokemon } = require("../db/sequelize");
 
 module.exports = (app) => {
-  app.get("api/pokemons", (req, res) => {
+  app.get("/api/pokemons", (req, res) => {
     Pokemon.findAll().then((pokemons) => {
-      const message = "La liste des pokémons a bien été récupérée";
+      const message = "La liste des pokémons a bien été récupérée.";
       res.json({ message, data: pokemons });
     });
   });
