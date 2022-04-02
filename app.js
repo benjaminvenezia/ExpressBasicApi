5047655aa0d6702286e9e2f690bd8c267c892fcf
benjamin <benjamin@pc-57.home>
2022-04-02 20:41:16 +0200

    finish clean up removing useless code

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const express = require("express");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
-const { Sequelize, DataTypes } = require("sequelize");
-const { success, getUniqueId } = require("./helper.js");
+const sequelize = require("./src/db/sequelize");
 
-let pokemons = require("./src/db/mock-pokemon");
-const PokemonModel = require("./src/models/pokemon");
-const { rsort } = require("semver");
-
-//On crée une instance d'express, il s'agit d'un petit serveur web sur lequel tournera l'api rest.
 const app = express();
-//Port sur lequel tournera l'api rest
 const port = 3000;
-
 
 //Middleware
 app
@@ -22,7 +14,10 @@ app
   .use(morgan("dev"))
   .use(bodyParser.json());
 
-//On démarre l'api rest sur le port 3000 et on affiche un message de log dans le terminal de commande.
+sequelize.initDb();
+
+//Ici nous placerons nos futurs endpoints
+
 app.listen(
   port,
   console.log(
