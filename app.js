7568eda403f9926ed173c1896204cfafa17ac09c
benjamin <benjamin@pc-57.home>
2022-04-02 20:38:30 +0200

    remove sequelize in app.js

--- app.js
+++ app.js
@@ -15,41 +15,6 @@ const app = express();
 //Port sur lequel tournera l'api rest
 const port = 3000;
 
-//initialisation de l'orm
-const sequelize = new Sequelize("pokedex", "root", "", {
-  host: "localhost",
-  dialect: "mariadb",
-  dialectOptions: {
-    timezone: "Etc/GMT-2",
-  },
-  logging: false,
-});
-
-sequelize
-  .authenticate()
-  .then((_) =>
-    console.log("La connexion à la base de données a bien été établie.")
-  )
-  .catch((error) =>
-    console.error(`Impossible de se connecter à la base de données : ${error}`)
-  );
-
-//modèle
-const Pokemon = PokemonModel(sequelize, DataTypes);
-
-sequelize.sync({ force: true }).then((_) => {
-  console.log('La base de données "Pokedex a bien été synchronisée');
-
-  pokemons.map((pokemon) => {
-    Pokemon.create({
-      name: pokemon.name,
-      hp: pokemon.hp,
-      cp: pokemon.cp,
-      picture: pokemon.picture,
-      types: pokemon.types.join(),
-    }).then((bulbizarre) => console.log(bulbizarre.toJSON()));
-  });
-});
 
 //Middleware
 app
