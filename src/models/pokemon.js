e34995af38c785149c431c0f12d206adbb873476
benjamin <benjamin@macbook-pro-de-benjamin.home>
2022-04-03 22:00:48 +0200

    add validators (code verification)

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,3 +1,15 @@
+const validTypes = [
+  "Plante",
+  "Poison",
+  "Feu",
+  "Eau",
+  "Insecte",
+  "Vol",
+  "Normal",
+  "Electrik",
+  "Fée",
+];
+
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "Pokemon",
@@ -10,27 +22,95 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Le champ name ne peut être nul!",
+          },
+          notNull: { msg: "Le name est une propriété requise." },
+          min: {
+            args: [1],
+            msg: "Le minimum de name est 0",
+          },
+          max: {
+            args: [25],
+            msg: "Le maximum de name est 25",
+          },
+        },
       },
       hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          isInt: {
+            msg: "Utilisez uniquement des nombres entiers pour les points de vie.",
+          },
+          notNull: { msg: "Les points de vie sont une propriété requise." },
+          min: {
+            args: [0],
+            msg: "Le minimum de HP est 0",
+          },
+          max: {
+            args: [999],
+            msg: "Le maximum de HP est 999",
+          },
+        },
       },
       cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          isInt: {
+            msg: "Utilisez uniquement des nombres entiers pour le cp",
+          },
+          notNull: { msg: "Le cp est une propriété requise." },
+          min: {
+            args: [0],
+            msg: "Le minimum de CP est 0",
+          },
+          max: {
+            args: [99],
+            msg: "Le maximum de CP est 99",
+          },
+        },
       },
       picture: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isUrl: {
+            msg: "Merci de renseigner une adresse url valide.",
+          },
+          notNull: { msg: "L'url est une propriété requise." },
+        },
       },
       types: {
         type: DataTypes.STRING,
         allowNull: false,
         get() {
-          return this.getDataValue('types').split(',')
+          return this.getDataValue("types").split(",");
         },
         set(types) {
-          this.setDataValue('types', types.join())
+          this.setDataValue("types", types.join());
+        },
+        validate: {
+          isTypesValid(value) {
+            //isTypesValid: nom arbitraire donné à notre validateur personnalisé
+            if (!value) {
+              throw new Error("Un pokémon doit au moins avoir un type");
+            }
+            if (value.split(",").length > 3) {
+              throw new Error(
+                "Un pokémon ne peux pas avoir plus de trois types"
+              );
+            }
+            value.split(",").forEach((type) => {
+              if (!validTypes.includes(type)) {
+                throw new Error(
+                  `Le type d'un pokémon doit appartenir à la liste suivante : ${validTypes}`
+                );
+              }
+            });
+          },
         },
       },
     },
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,4 +1,5 @@
 const { Pokemon } = require("../db/sequelize");
+const { ValidationError } = require("sequelize");
 
 module.exports = (app) => {
   app.put("/api/pokemons/:id", (req, res) => {
@@ -12,13 +13,16 @@ module.exports = (app) => {
             if (pokemon === null) {
               const message =
                 "Le pokémon demandé n'existe pas, Réessayez avec un autre identifiant.";
-              return res.statut(404).json(message);
+              return res.status(404).json(message);
             }
             const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
             res.json({ message, data: pokemon });
           });
       })
       .catch((error) => {
+        if (error instanceof ValidationError) {
+          return res.status(400).json({ message: error.message, data: error });
+        }
         const message =
           "Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants";
         res.status(500).json({ message, data: error });
