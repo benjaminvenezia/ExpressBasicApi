0b67fe5b35611182a5f348e94b15f0afdc95948b
benjamin <benjamin@macbook-pro-de-benjamin.home>
2022-04-04 23:18:46 +0200

    add auth

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,6 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const PokemonModel = require("../models/pokemon");
+const UserModel = require("../models/user");
 const pokemons = require("./mock-pokemon");
+const bcrypt = require("bcrypt");
 
 const sequelize = new Sequelize("pokedex", "root", "", {
   host: "localhost",
@@ -12,6 +14,7 @@ const sequelize = new Sequelize("pokedex", "root", "", {
 });
 
 const Pokemon = PokemonModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
   return sequelize.sync({ force: true }).then((_) => {
@@ -21,14 +24,21 @@ const initDb = () => {
         hp: pokemon.hp,
         cp: pokemon.cp,
         picture: pokemon.picture,
-        types: pokemon.types
+        types: pokemon.types,
       }).then((pokemon) => console.log(pokemon.toJSON()));
     });
-    console.log("La base de donnée a bien été initialisée !");
+
+    bcrypt
+      .hash("pikachu", 10)
+      .then((hash) => User.create({ username: "pikachu", password: hash }))
+      .then((user) => console.log(user.toJSON()));
+
+    console.log("La base de donnée a bien été instanciée !");
   });
 };
 
 module.exports = {
   initDb,
   Pokemon,
+  User,
 };
